a0cdc2dd6a970b45da5d24b7d25ea0b301e01695
Marc Clapera <marc.clapera@ericsson.com>
2022-01-23 13:10:49 +0200

    corrected UI refresh bug

--- Src/Infrastructure.CoinbaseExchangeProvider/Models/CoinbaseExchangeResponse.cs
+++ Src/Infrastructure.CoinbaseExchangeProvider/Models/CoinbaseExchangeResponse.cs
@@ -18,6 +18,6 @@ namespace Infrastructure.CoinbaseExchangeProvider.Models
         public string? TargetCurrency { get; set; }
 
         [JsonPropertyName("amount")]
-        public string? Price { get; set; }
+        public string Price { get; set; } = "0";
     }
 }
